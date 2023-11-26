7558bb5f92d314ee8b9e4ebfbcd3205b9143535c
Roman Krotov <krotoroman@gmail.com>
2023-11-26 15:23:28 +0300

    done

---
+++ js/swiper.js
@@ -0,0 +1,15 @@
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+
+
+    effect: 'slide',
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+});
